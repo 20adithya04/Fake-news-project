f6a19b6401305b9a978fb0efb949b9e5f1a1abc1
20adithya04 <20adithya04@gmail.com>
2026-09-10 15:43:29 +0530

    Fix GitHub Pages routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router basename="/Fake-news-project">
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/history" element={<History />} />
